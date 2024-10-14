a65aa6785755d704dcd0821393a590501b6094ed
Alexandru CIOBOTARU <aciobotaru051@gmail.com>
2024-10-14 09:39:01 +0200

    Deplacé les boutons enregistrer et garder la meme taille

--- CreditCeleste/frmVoiture.Designer.cs
+++ CreditCeleste/frmVoiture.Designer.cs
@@ -57,9 +57,11 @@
             this.gpbAgeVehicule.Controls.Add(this.rdbOccas3a5);
             this.gpbAgeVehicule.Controls.Add(this.rdbOccasMoins3);
             this.gpbAgeVehicule.Controls.Add(this.rdbNeuf);
-            this.gpbAgeVehicule.Location = new System.Drawing.Point(250, 29);
+            this.gpbAgeVehicule.Location = new System.Drawing.Point(333, 36);
+            this.gpbAgeVehicule.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gpbAgeVehicule.Name = "gpbAgeVehicule";
-            this.gpbAgeVehicule.Size = new System.Drawing.Size(218, 117);
+            this.gpbAgeVehicule.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gpbAgeVehicule.Size = new System.Drawing.Size(291, 144);
             this.gpbAgeVehicule.TabIndex = 9;
             this.gpbAgeVehicule.TabStop = false;
             this.gpbAgeVehicule.Text = "Age du véhicule";
@@ -67,9 +69,10 @@
             // rdbOccas5OuPlus
             // 
             this.rdbOccas5OuPlus.AutoSize = true;
-            this.rdbOccas5OuPlus.Location = new System.Drawing.Point(6, 89);
+            this.rdbOccas5OuPlus.Location = new System.Drawing.Point(8, 110);
+            this.rdbOccas5OuPlus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbOccas5OuPlus.Name = "rdbOccas5OuPlus";
-            this.rdbOccas5OuPlus.Size = new System.Drawing.Size(136, 17);
+            this.rdbOccas5OuPlus.Size = new System.Drawing.Size(177, 21);
             this.rdbOccas5OuPlus.TabIndex = 3;
             this.rdbOccas5OuPlus.Text = "Occasion 5 ans ou plus";
             this.rdbOccas5OuPlus.UseVisualStyleBackColor = true;
@@ -77,9 +80,10 @@
             // rdbOccas3a5
             // 
             this.rdbOccas3a5.AutoSize = true;
-            this.rdbOccas3a5.Location = new System.Drawing.Point(6, 66);
+            this.rdbOccas3a5.Location = new System.Drawing.Point(8, 81);
+            this.rdbOccas3a5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbOccas3a5.Name = "rdbOccas3a5";
-            this.rdbOccas3a5.Size = new System.Drawing.Size(132, 17);
+            this.rdbOccas3a5.Size = new System.Drawing.Size(171, 21);
             this.rdbOccas3a5.TabIndex = 2;
             this.rdbOccas3a5.Text = "Occasion de 3 à 5 ans";
             this.rdbOccas3a5.UseVisualStyleBackColor = true;
@@ -87,9 +91,10 @@
             // rdbOccasMoins3
             // 
             this.rdbOccasMoins3.AutoSize = true;
-            this.rdbOccasMoins3.Location = new System.Drawing.Point(6, 42);
+            this.rdbOccasMoins3.Location = new System.Drawing.Point(8, 52);
+            this.rdbOccasMoins3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbOccasMoins3.Name = "rdbOccasMoins3";
-            this.rdbOccasMoins3.Size = new System.Drawing.Size(120, 17);
+            this.rdbOccasMoins3.Size = new System.Drawing.Size(156, 21);
             this.rdbOccasMoins3.TabIndex = 1;
             this.rdbOccasMoins3.Text = "Occasion - de 3 ans";
             this.rdbOccasMoins3.UseVisualStyleBackColor = true;
@@ -97,53 +102,60 @@
             // rdbNeuf
             // 
             this.rdbNeuf.AutoSize = true;
-            this.rdbNeuf.Location = new System.Drawing.Point(6, 19);
+            this.rdbNeuf.Location = new System.Drawing.Point(8, 23);
+            this.rdbNeuf.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdbNeuf.Name = "rdbNeuf";
-            this.rdbNeuf.Size = new System.Drawing.Size(48, 17);
+            this.rdbNeuf.Size = new System.Drawing.Size(59, 21);
             this.rdbNeuf.TabIndex = 0;
             this.rdbNeuf.Text = "Neuf";
             this.rdbNeuf.UseVisualStyleBackColor = true;
             // 
             // txtNumImmat
             // 
-            this.txtNumImmat.Location = new System.Drawing.Point(142, 29);
+            this.txtNumImmat.Location = new System.Drawing.Point(189, 36);
+            this.txtNumImmat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNumImmat.Name = "txtNumImmat";
-            this.txtNumImmat.Size = new System.Drawing.Size(100, 20);
+            this.txtNumImmat.Size = new System.Drawing.Size(132, 22);
             this.txtNumImmat.TabIndex = 14;
             // 
             // txtNouveauVhc
             // 
-            this.txtNouveauVhc.Location = new System.Drawing.Point(369, 5);
+            this.txtNouveauVhc.Location = new System.Drawing.Point(492, 6);
+            this.txtNouveauVhc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNouveauVhc.Name = "txtNouveauVhc";
-            this.txtNouveauVhc.Size = new System.Drawing.Size(100, 20);
+            this.txtNouveauVhc.Size = new System.Drawing.Size(132, 22);
             this.txtNouveauVhc.TabIndex = 13;
             // 
             // txtDate1ereImat
             // 
-            this.txtDate1ereImat.Location = new System.Drawing.Point(142, 5);
+            this.txtDate1ereImat.Location = new System.Drawing.Point(189, 6);
+            this.txtDate1ereImat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtDate1ereImat.Name = "txtDate1ereImat";
-            this.txtDate1ereImat.Size = new System.Drawing.Size(100, 20);
+            this.txtDate1ereImat.Size = new System.Drawing.Size(132, 22);
             this.txtDate1ereImat.TabIndex = 12;
             // 
             // txtPuissance
             // 
-            this.txtPuissance.Location = new System.Drawing.Point(142, 78);
+            this.txtPuissance.Location = new System.Drawing.Point(189, 96);
+            this.txtPuissance.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPuissance.Name = "txtPuissance";
-            this.txtPuissance.Size = new System.Drawing.Size(100, 20);
+            this.txtPuissance.Size = new System.Drawing.Size(132, 22);
             this.txtPuissance.TabIndex = 11;
             // 
             // txtNumSerie
             // 
-            this.txtNumSerie.Location = new System.Drawing.Point(142, 54);
+            this.txtNumSerie.Location = new System.Drawing.Point(189, 66);
+            this.txtNumSerie.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNumSerie.Name = "txtNumSerie";
-            this.txtNumSerie.Size = new System.Drawing.Size(100, 20);
+            this.txtNumSerie.Size = new System.Drawing.Size(132, 22);
             this.txtNumSerie.TabIndex = 15;
             // 
             // btnValider
             // 
-            this.btnValider.Location = new System.Drawing.Point(394, 154);
+            this.btnValider.Location = new System.Drawing.Point(441, 188);
+            this.btnValider.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnValider.Name = "btnValider";
-            this.btnValider.Size = new System.Drawing.Size(75, 20);
+            this.btnValider.Size = new System.Drawing.Size(183, 24);
             this.btnValider.TabIndex = 18;
             this.btnValider.Text = "Valider";
             this.btnValider.UseVisualStyleBackColor = true;
@@ -151,9 +163,10 @@
             // 
             // btnInfo
             // 
-            this.btnInfo.Location = new System.Drawing.Point(12, 154);
+            this.btnInfo.Location = new System.Drawing.Point(16, 188);
+            this.btnInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnInfo.Name = "btnInfo";
-            this.btnInfo.Size = new System.Drawing.Size(74, 20);
+            this.btnInfo.Size = new System.Drawing.Size(100, 24);
             this.btnInfo.TabIndex = 17;
             this.btnInfo.Text = "Introduction";
             this.btnInfo.UseVisualStyleBackColor = true;
@@ -161,9 +174,10 @@
             // 
             // btnEnregistre
             // 
-            this.btnEnregistre.Location = new System.Drawing.Point(318, 154);
+            this.btnEnregistre.Location = new System.Drawing.Point(333, 188);
+            this.btnEnregistre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEnregistre.Name = "btnEnregistre";
-            this.btnEnregistre.Size = new System.Drawing.Size(74, 20);
+            this.btnEnregistre.Size = new System.Drawing.Size(100, 24);
             this.btnEnregistre.TabIndex = 16;
             this.btnEnregistre.Text = "Enregistrer";
             this.btnEnregistre.UseVisualStyleBackColor = true;
@@ -172,71 +186,78 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(248, 7);
+            this.label1.Location = new System.Drawing.Point(331, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.Size = new System.Drawing.Size(154, 17);
             this.label1.TabIndex = 19;
             this.label1.Text = "Mon Nouveau Véhicule";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 7);
+            this.label2.Location = new System.Drawing.Point(13, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.Size = new System.Drawing.Size(169, 17);
             this.label2.TabIndex = 20;
             this.label2.Text = "Date 1ère Immatriculation";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(10, 32);
+            this.label3.Location = new System.Drawing.Point(13, 39);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(117, 13);
+            this.label3.Size = new System.Drawing.Size(156, 17);
             this.label3.TabIndex = 21;
             this.label3.Text = "Numéro Immatriculation";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(10, 56);
+            this.label4.Location = new System.Drawing.Point(13, 69);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.Size = new System.Drawing.Size(113, 17);
             this.label4.TabIndex = 22;
             this.label4.Text = "Numéro de série";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(10, 80);
+            this.label5.Location = new System.Drawing.Point(13, 98);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.Size = new System.Drawing.Size(73, 17);
             this.label5.TabIndex = 23;
             this.label5.Text = "Puissance";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(10, 120);
+            this.label6.Location = new System.Drawing.Point(13, 148);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.Size = new System.Drawing.Size(62, 17);
             this.label6.TabIndex = 24;
             this.label6.Text = "Vendeur";
             // 
             // lblVendeur
             // 
             this.lblVendeur.AutoSize = true;
-            this.lblVendeur.Location = new System.Drawing.Point(86, 120);
+            this.lblVendeur.Location = new System.Drawing.Point(115, 148);
+            this.lblVendeur.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblVendeur.Name = "lblVendeur";
-            this.lblVendeur.Size = new System.Drawing.Size(12, 13);
+            this.lblVendeur.Size = new System.Drawing.Size(12, 17);
             this.lblVendeur.TabIndex = 25;
             this.lblVendeur.Text = "/";
             // 
             // frmVoiture
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(481, 186);
+            this.ClientSize = new System.Drawing.Size(641, 229);
             this.Controls.Add(this.lblVendeur);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -254,6 +275,7 @@
             this.Controls.Add(this.txtPuissance);
             this.Controls.Add(this.gpbAgeVehicule);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "frmVoiture";
             this.ShowIcon = false;
